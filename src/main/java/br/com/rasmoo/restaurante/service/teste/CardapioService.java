091311ba4d5eabcb59a6383bf6ea9de8bc64fe51
silluana <silluana@gmail.com>
2025-03-17 15:28:26 -0300

    Alterações no Cardapio Service

--- src/main/java/br/com/rasmoo/restaurante/service/teste/CardapioService.java
+++ src/main/java/br/com/rasmoo/restaurante/service/teste/CardapioService.java
@@ -1,48 +1,71 @@
 package br.com.rasmoo.restaurante.service.teste;
 
-import br.com.rasmoo.restaurante.dao.PratoDao;
-import br.com.rasmoo.restaurante.entity.Prato;
+import br.com.rasmoo.restaurante.dao.CardapioDao;
+import br.com.rasmoo.restaurante.dao.CategoriaDao;
+import br.com.rasmoo.restaurante.entity.Cardapio;
+import br.com.rasmoo.restaurante.entity.Categoria;
 import br.com.rasmoo.restaurante.util.JPAUtil;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
 
-public class PratoService {
+public class CardapioService {
 
     public static void main(String[] args) {
 
-        Prato camaraoAbacaxi = new Prato();
+        EntityManager entityManager = JPAUtil.getEntityManagerCardapioLog();
+        cadastrarProdutoCardapio(entityManager, cadastrarCategoria(entityManager));
+    }
+
+    private static  Categoria cadastrarCategoria(EntityManager entityManager) {
+        CategoriaDao categoriaDao = new CategoriaDao(entityManager);
+        Categoria pratoPrincipal = new Categoria("Prato Principal");
+
+        entityManager.getTransaction().begin();
+        categoriaDao.cadastrar(pratoPrincipal);
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+
+        return pratoPrincipal;
+    }
+
+    private static void cadastrarProdutoCardapio(EntityManager entityManager, Categoria categoria) {
+        Cardapio camaraoAbacaxi = new Cardapio();
         camaraoAbacaxi.setNome("CAMARÃO NO ABACAXI");
         camaraoAbacaxi.setDescricao("Abacaxi recheado com creme de catupiry, creem cheese e camarão. Coberto por " +
                 "camarão VM e queijo parmesão. Acompanha batata palha, palmito, azeitona e arroz branco.");
         camaraoAbacaxi.setDisponivel(true);
         camaraoAbacaxi.setValor(BigDecimal.valueOf(160.00));
+        camaraoAbacaxi.setCategoria(categoria);
 
-        Prato camaraoCoco = new Prato();
+        Cardapio camaraoCoco = new Cardapio();
         camaraoCoco.setNome("CAMARÃO NO COCO");
         camaraoCoco.setDescricao("Coco verde recheado com creme de catupiry, creem cheese e camarão. Coberto por " +
                 "camarão VM e queijo parmesão. Acompanha batata palha, palmito, azeitona e arroz branco.");
         camaraoCoco.setDisponivel(true);
         camaraoCoco.setValor(BigDecimal.valueOf(150.00));
+        camaraoCoco.setCategoria(categoria);
 
-        EntityManager entityManager = JPAUtil.getEntityManagerCardapioLog();
-        PratoDao pratoDao = new PratoDao(entityManager);
+        CardapioDao cardapioDao = new CardapioDao(entityManager);
         entityManager.getTransaction().begin();
-        pratoDao.cadastrar(camaraoAbacaxi);
-        entityManager.flush();
-        pratoDao.cadastrar(camaraoCoco);
-        entityManager.flush();
-        System.out.println("O prato consultado: "+pratoDao.consultar(2));
 
-        pratoDao.excluir(camaraoCoco);
-        System.out.println("O prato consultado: "+pratoDao.consultar(2));
+        cardapioDao.cadastrar(camaraoAbacaxi);
+        entityManager.flush();
+        cardapioDao.cadastrar(camaraoCoco);
+        entityManager.flush();
+        //System.out.println("O prato consultado: "+ cardapioDao.consultarPorId(2));
+
+        cardapioDao.consultarTodos().forEach(elemento->System.out.println("O prato consultado foi: "+elemento));
+
+        //cardapioDao.excluir(camaraoCoco);
+        //System.out.println("O prato consultado: "+ cardapioDao.consultar(2));
         //entityManager.getTransaction().commit();
-        //entityManager.close();
-        entityManager.clear();
+        entityManager.close();
+        //entityManager.clear();
 
-        camaraoAbacaxi.setValor(BigDecimal.valueOf(125.50));
-        pratoDao.atualizar(camaraoAbacaxi);
-        System.out.println("O prato consultado: "+pratoDao.consultar(1));
+        //camaraoAbacaxi.setValor(BigDecimal.valueOf(125.50));
+        //cardapioDao.atualizar(camaraoAbacaxi);
+        //System.out.println("O prato consultado: "+ cardapioDao.consultarPorId(1));
     }
 
 }
