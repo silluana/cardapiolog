6b54d87866123122fd89f5a4105e3089f0f1419a
silluana <silluana@gmail.com>
2025-03-20 16:43:31 -0300

    Fechando o treinamento de JPA Hibernate com a criação de consulta utilizando Criteria e entendo sobre as classes Embeddable

--- src/main/java/br/com/rasmoo/restaurante/dao/EnderecoDao.java
+++ src/main/java/br/com/rasmoo/restaurante/dao/EnderecoDao.java
@@ -5,6 +5,10 @@ import br.com.rasmoo.restaurante.vo.ClienteVo;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Objects;
 
@@ -30,7 +34,7 @@ public class EnderecoDao {
     }
 
     public List<ClienteVo> consultarClientes(final String estado, final String cidade, final String rua) {
-        String jpql = "SELECT new br.com.rasmoo.restaurante.vo.ClienteVo(e.cliente.cpf,e.cliente.nome) " +
+        String jpql = "SELECT new br.com.rasmoo.restaurante.vo.ClienteVo(e.cliente.clienteId.cpf,e.cliente.nome) " +
                 "FROM Endereco e WHERE 1=1";
         if (Objects.nonNull(estado)) {
             jpql = jpql.concat("AND UPPER(e.estado) = UPPER(:estado) ");
@@ -54,6 +58,28 @@ public class EnderecoDao {
         }
 
         return typedQuery.getResultList();
+    }
+
+    public List<ClienteVo> consultarClientesCriteria(final String estado, final String cidade, final String rua) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<ClienteVo> criteriaQuery = builder.createQuery(ClienteVo.class);
+        Root<Endereco> root = criteriaQuery.from(Endereco.class);
+        criteriaQuery.multiselect(root.get("cliente").get("clienteId").get("cpf"), root.get("cliente").get("nome"));
+        Predicate predicate = builder.and();
+
+        if (Objects.nonNull(estado)) {
+            predicate = builder.and(predicate, builder.equal(builder.upper(root.get("estado")), estado.toUpperCase()));
+        }
+        if (Objects.nonNull(cidade)) {
+            predicate = builder.and(predicate, builder.equal(builder.upper(root.get("cidade")), cidade.toUpperCase()));
+        }
+        if (Objects.nonNull(rua)) {
+            predicate = builder.and(predicate, builder.equal(builder.upper(root.get("rua")), rua.toUpperCase()));
+        }
+
+        criteriaQuery.where(predicate);
+
+        return entityManager.createQuery(criteriaQuery).getResultList();
     }
 
     public void atualizar(final Endereco endereco) {
--- src/main/java/br/com/rasmoo/restaurante/service/teste/OrdemService.java
+++ src/main/java/br/com/rasmoo/restaurante/service/teste/OrdemService.java
@@ -4,10 +4,7 @@ import br.com.rasmoo.restaurante.dao.CardapioDao;
 import br.com.rasmoo.restaurante.dao.ClienteDao;
 import br.com.rasmoo.restaurante.dao.EnderecoDao;
 import br.com.rasmoo.restaurante.dao.OrdemDao;
-import br.com.rasmoo.restaurante.entity.Cliente;
-import br.com.rasmoo.restaurante.entity.Endereco;
-import br.com.rasmoo.restaurante.entity.Ordem;
-import br.com.rasmoo.restaurante.entity.OrdensCardapio;
+import br.com.rasmoo.restaurante.entity.*;
 import br.com.rasmoo.restaurante.util.CargaDeDadosUtil;
 import br.com.rasmoo.restaurante.util.JPAUtil;
 
@@ -24,12 +21,15 @@ public class OrdemService {
         CargaDeDadosUtil.cadastrarOrdensClientes(entityManager);
 
         //OrdemDao ordemDao = new OrdemDao(entityManager);
-        //ClienteDao clienteDao = new ClienteDao(entityManager);
+        ClienteDao clienteDao = new ClienteDao(entityManager);
         EnderecoDao enderecoDao = new EnderecoDao(entityManager);
         //System.out.println(ordemDao.consultarItensMaisVendidos());
         //System.out.println(clienteDao.consultarTodos());
 
         System.out.println(enderecoDao.consultarClientes(null, null, "Lapa"));
+        System.out.println(enderecoDao.consultarClientesCriteria(null, null, "Lapa"));
+
+        System.out.println(clienteDao.consultarPorId(new ClienteId("claudia@email.com", "111111111345")));
 
         //Ordem ordem  =  ordemDao.joinFetchCliente(2);
         //Ordem ordem  =  ordemDao.consultarPorId(2);
